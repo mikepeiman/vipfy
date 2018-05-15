aff0226547cced792c6dab5fd0f97b119bb456db
Mike Peiman <mikepeiman@gmail.com>
2018-05-15 10:56:39 -0700

    Added some sales copy to first page of view (my-view1.js)

--- src/components/my-view1.js
+++ src/components/my-view1.js
@@ -16,7 +16,17 @@ class MyView1 extends PageViewElement {
   _render(props) {
     return html`
       ${SharedStyles}
-
+		<section>
+        <h2>VIP For You</h2>
+        <h3>You deserve the VIP treatment; we make it as easy as the click of a button!</h3>
+        <p>VIPFY ("Vipify") is the world's first platform to arbitrage the one thing we all have a finite supply of: time.
+        Have you ever wasted your valuable time waiting in line? Running errands when you had better things to do?
+        Wait no longer! Vipify yourself - VIP's don't wait in line. VIP's don't run petty errands, unless they want to.
+        Explore some examples of <a href="#">our services</a>, <a href="#">locations</a>, or <a href="#">post a request</a> to have one of our Humble Servants complete
+        your task for you.</p>
+        <h3>No more need to wait - welcome to the life of a V.I.P.!</h3>
+        <br><br>
+      </section>
     `;
   }
 }
